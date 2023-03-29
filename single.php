d561ed6019a7143fedde5c92507cdcacb386ad16
Aleksandar Perisic <aleksandar.perisic@regiondo.com>
2023-03-29 15:40:46 +0200

    index, single

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php
+// header
+get_header();
+?>
+	<h1><?php bloginfo( 'name' ); ?></h1>
+	<h2><?php bloginfo( 'description' ); ?></h2>
+
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<h3><?php the_title(); ?></h3>
+		<div class="content">
+			<?php the_content(); ?>
+		</div>
+
+	<?php endwhile; ?>
+<?php endif; ?>
+
+<?php
+// footer
+wp_footer();

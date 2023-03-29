4db56a66dfec37c3aecaaf54b2e2ef21fdd538fe
Aleksandar Perisic <aleksandar.perisic@regiondo.com>
2023-03-29 17:14:06 +0200

    single 🎯

--- single.php
+++ single.php
@@ -2,18 +2,23 @@
 // header
 get_header();
 ?>
-	<h1><?php bloginfo( 'name' ); ?></h1>
-	<h2><?php bloginfo( 'description' ); ?></h2>
+
+<div class="container">
+    <div class="row">
+        <h1 class="mb-5"><?php bloginfo( 'name' ); ?></h1>
+    </div>
+</div>
 
 <?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
+    <div class="container">
+        <div class="row">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-		<h3><?php the_title(); ?></h3>
-		<div class="content">
-			<?php the_content(); ?>
-		</div>
+				<?php get_template_part( 'template-parts/card', get_post_type() ); ?>
 
-	<?php endwhile; ?>
+			<?php endwhile; ?>
+        </div>
+    </div>
 <?php endif; ?>
 
 <?php
